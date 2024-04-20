8f8b1d28cac5d4469b246274056a555b2d969e74
hb351 <boustta.elh@gmail.com>
2024-04-20 19:48:19 +0100

    add the predict path and function and load the model

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, Request, jsonify
+import pickle
+import sklearn
+
+print(sklearn.__version__)
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Welcome to the Student Success Prediction Service!"
+
+#load the model
+with open("./grid_search_rfc.pkl", 'rb') as model_file:
+    model = pickle.load(model_file)
+
+#define the prediction end point
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = Request.get_json(force=True)
+    features = [
+        data['Course'], data['Daytime/evening attendance'], data['Previous qualification'],
+        data['Previous qualification (grade)'], data['Admission grade'],
+        data['Educational special needs'], data['Tuition fees up to date'], data['Gender'],
+        data['Scholarship holder'], data['Age at enrollment'],
+        data['Curricular units 1st sem (credited)'],
+        data['Curricular units 1st sem (enrolled)'],
+        data['Curricular units 1st sem (evaluations)'],
+        data['Curricular units 1st sem (approved)'],
+        data['Curricular units 1st sem (grade)'],
+        data['Curricular units 1st sem (without evaluations)'],
+        data['Curricular units 2nd sem (credited)'],
+        data['Curricular units 2nd sem (enrolled)'],
+        data['Curricular units 2nd sem (evaluations)'],
+        data['Curricular units 2nd sem (approved)'],
+        data['Curricular units 2nd sem (grade)'],
+        data['Curricular units 2nd sem (without evaluations)']
+    ]
+
+    prediction = model.predict(features)
+    return jsonify({"prediction: ": prediction.tolist()})
 
 if __name__ == "__main__":
     app.run(debug=True)
