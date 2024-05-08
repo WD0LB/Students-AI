4cd9f999db9d741fb4ac3743306b1576926a418b
oxhb <boustta.elh@gmail.com>
2024-05-08 21:39:09 +0100

    add the result page and changed logic for redirections

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 import pickle
 
 app = Flask(__name__, static_folder='static')
@@ -18,29 +18,45 @@ with open('/home/hb/projects/Students-AI/app/grid_search_rfc.pkl', 'rb') as mode
 #define the prediction end point
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json(force=True)
-    features = [
-        data['Course'], data['Daytime/evening attendance'], data['Previous qualification'],
-        data['Previous qualification (grade)'], data['Admission grade'],
-        data['Educational special needs'], data['Tuition fees up to date'], data['Gender'],
-        data['Scholarship holder'], data['Age at enrollment'],
-        data['Curricular units 1st sem (credited)'],
-        data['Curricular units 1st sem (enrolled)'],
-        data['Curricular units 1st sem (evaluations)'],
-        data['Curricular units 1st sem (approved)'],
-        data['Curricular units 1st sem (grade)'],
-        data['Curricular units 1st sem (without evaluations)'],
-        data['Curricular units 2nd sem (credited)'],
-        data['Curricular units 2nd sem (enrolled)'],
-        data['Curricular units 2nd sem (evaluations)'],
-        data['Curricular units 2nd sem (approved)'],
-        data['Curricular units 2nd sem (grade)'],
-        data['Curricular units 2nd sem (without evaluations)']
-    ]
-    print(features)
+    # Assuming form data is used instead of JSON
+    try:
+        features = [
+            int(request.form['Course']),
+            int(request.form['Daytime/evening attendance']),
+            int(request.form['Previous qualification']),
+            float(request.form['Previous qualification (grade)']),
+            float(request.form['Admission grade']),
+            int(request.form['Educational special needs']),
+            int(request.form['Tuition fees up to date']),
+            int(request.form['Gender']),
+            int(request.form['Scholarship holder']),
+            int(request.form['Age at enrollment']),
+            int(request.form['Curricular units 1st sem (credited)']),
+            int(request.form['Curricular units 1st sem (enrolled)']),
+            int(request.form['Curricular units 1st sem (evaluations)']),
+            int(request.form['Curricular units 1st sem (approved)']),
+            float(request.form['Curricular units 1st sem (grade)']),
+            int(request.form['Curricular units 1st sem (without evaluations)']),
+            int(request.form['Curricular units 2nd sem (credited)']),
+            int(request.form['Curricular units 2nd sem (enrolled)']),
+            int(request.form['Curricular units 2nd sem (evaluations)']),
+            int(request.form['Curricular units 2nd sem (approved)']),
+            float(request.form['Curricular units 2nd sem (grade)']),
+            int(request.form['Curricular units 2nd sem (without evaluations)'])
+        ]
+        
+        prediction = model.predict([features])
+        # Redirect to the results page with prediction result
+        return redirect(url_for('result', prediction=prediction[0]))
 
-    prediction = model.predict([features])
-    return jsonify({"prediction: ": prediction.tolist()})
+    except Exception as e:
+        return str(e)  # For debugging purposes
+    
+@app.route('/result')
+def result():
+    prediction = request.args.get('prediction', type=str)  # Retrieve the prediction from query parameters
+    return render_template('result.html', prediction=prediction)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
